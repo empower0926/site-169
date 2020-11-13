68c0323f1531315a5df243f8a5b5f89e91027c49
merlinDev <wanipun.work@gmail.com>
2020-11-13 20:35:53 +0530

    fetched data

--- assets/app.js
+++ assets/app.js
@@ -3,17 +3,42 @@ let slideIndex = 0;
 let colors = ['red', 'green', 'blue'];
 
 const feedURL = 'https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fflashgroupnews.com%2Ffeed';
-const connection = new XMLHttpRequest();
-let jsonObject;
+const bitcoinFeedURL = 'https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fflashgroupnews.com%2Fcategory%2Fbitcoin%2Ffeed';
 
-connection.open('GET', feedURL, true);
-connection.send();
+setData(bitcoinFeedURL).then((val) => {
+    let bitcoinNews = JSON.parse(val);
+    console.log(bitcoinNews);
+});
 
-connection.onreadystatechange = (e) => {
-    if (connection.readyState === 4) {
-        jsonObject = JSON.parse(connection.responseText);
-        console.log(jsonObject.items);
+setData(feedURL).then((val) => {
+    let feed = JSON.parse(val);
+    console.log(feed);
+});
+
+async function setData(url) {
+    try{
+        let data = await getFeed(url);
+        return data;
+    }catch(err){
+        console.log(err);
     }
+}
+
+async function getFeed(url) {
+    let connection = new XMLHttpRequest();
+    return new Promise((resolve, reject) => {
+        connection.onreadystatechange = function () {
+            if (connection.readyState === 4) {
+                if (connection.status >= 300) {
+                    reject('error: ' + status.code);
+                } else {
+                    resolve(connection.responseText);
+                }
+            }
+        }
+        connection.open('GET', url, true);
+        connection.send();
+    });
 }
 
 function slide() {
